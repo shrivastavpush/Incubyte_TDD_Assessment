81978c344f06a024313a47887b0e542f2f989fe2
Pushpendra Shrivastav <shrivastavsumit15@gmail.com>
2025-06-18 22:34:22 +0530

    Added First point test cases + Logic implementation

--- add.js
+++ add.js
@@ -1,5 +1,8 @@
-function add(a, b) {
-    return a + b
+function add(numbers) {
+    if (numbers === '') return 0;
+
+    const numberArray = numbers.split(',').map(Number);
+    return numberArray.reduce((sum, num) => sum + num, 0);
 }
 
-module.exports = add
+module.exports = add;
--- add.test.js
+++ add.test.js
@@ -1,5 +1,15 @@
-const add = require('./add')
+const add = require('./add');
 
-test('adds 1 + 2 to equal 3', () => {
-    expect(add(1, 2)).toBe(3);
+describe('String Calculator', () => {
+    test('returns 0 for an empty string', () => {
+        expect(add('')).toBe(0);
+    });
+
+    test('returns the number for a single number', () => {
+        expect(add('1')).toBe(1);
+    });
+
+    test('returns the sum of two numbers separated by comma', () => {
+        expect(add('2,2')).toBe(4);
+    });
 });
