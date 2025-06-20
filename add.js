6b3f571c3a4194599116732880ef7776dad9556b
Pushpendra Shrivastav <shrivastavsumit15@gmail.com>
2025-06-20 08:18:39 +0530

    Added 4th point Test Case + Logic

--- add.js
+++ add.js
@@ -1,8 +1,29 @@
 function add(numbers) {
     if (numbers === '') return 0;
 
-    // Split by either comma or newline
-    const numberArray = numbers.split(/[\n,]/).map(Number);
+    let delimiters = [',', '\n'];
+    let numbersToProcess = numbers;
+
+    // Check for custom delimiter
+    if (numbers.startsWith('//')) {
+        const delimiterEnd = numbers.indexOf('\n');
+        if (delimiterEnd !== -1) {
+            const delimiterPart = numbers.substring(2, delimiterEnd);
+            if (delimiterPart) {
+                delimiters = [delimiterPart];
+            }
+            numbersToProcess = numbers.substring(delimiterEnd + 1);
+        }
+    }
+
+    // Creating a regex pattern that matches any of the delimiters
+    const delimiterPattern = new RegExp(`[${delimiters.join('')}]`);
+
+    const numberArray = numbersToProcess
+        .split(delimiterPattern)
+        .filter(num => num !== '')
+        .map(Number);
+
     return numberArray.reduce((sum, num) => sum + num, 0);
 }
 
--- add.test.js
+++ add.test.js
@@ -2,30 +2,42 @@ const add = require('./add');
 
 describe('String Calculator', () => {
 
-    // point 1.1
+    // point 1.1 - handle empty string
     test('returns 0 for an empty string', () => {
         expect(add('')).toBe(0);
     });
 
-    // point 1.2
+    // point 1.2 - handle single number
     test('returns the number for a single number', () => {
         expect(add('1')).toBe(1);
     });
 
-    // point 1.3
+    // point 1.3 - handle two numbers separated by comma
     test('returns the sum of two numbers separated by comma', () => {
         expect(add('2,2')).toBe(4);
     });
 
-    // point 2
+    // point 2 - handle unknown amount of numbers
     test('returns the sum of an unknown amount of numbers', () => {
         expect(add('1,2,3,4,5')).toBe(15);
         expect(add('10,20,30,40,50,60')).toBe(210);
     });
 
-    // point 3
+    // point 3 - handle new lines between numbers
     test('handles new lines between numbers', () => {
         expect(add('1\n2,3')).toBe(6);
         expect(add('10\n20\n30,40,50')).toBe(150);
+    });
+
+    // point 4.1 - Custom delimiters
+    test('supports custom delimiters', () => {
+        expect(add('//;\n1;2')).toBe(3);
+        expect(add('//|\n1|2|3')).toBe(6);
+        expect(add('//\n\n1\n2')).toBe(3);  // Empty delimiter should default to newline
+    });
+
+    // point 4.2 - Multiple delimiters
+    test('handles multiple delimiters', () => {
+        expect(add('1,2\n3')).toBe(6);  // Using default delimiters
     });
 });
